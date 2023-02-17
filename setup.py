59695df05dfa23afd6d394e5d982ab88f96509d2
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-02-17 12:28:23 +0100

    build: bump version to 0.4.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='django-async-downloads',
-    version='0.4.5',
+    version='0.4.6',
     author='David Vaughan',
     author_email='david.vaughan@quickrelease.co.uk',
     maintainer="Quick Release (Automotive) Ltd.",
